5ab5dd6a8c675afd8e7169ef8d25b661bb1255e4
paolo <paolo@foo.com>
2025-10-28 20:58:10 +0100

    aggiunto esercizio 05

---
+++ 05_ESERCIZIO_classifica_con_array_associativi/00_classifica_associativi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="utf-8">
+    <title>00 classifica</title>
+  </head>
+  <body>
+    <header></header>
+    <main>
+
+    <?php
+        //definire array associativo con nomi squadre e punti
+        $classifica = array(
+            "Juventus" => 12,
+            "Inter"    => 45,
+            "Milan"    => 20,
+            "Roma"     => 21,
+            "Napoli"   => 33,
+            "Lazio"    => 18,
+            "Fiorentina" => 25)
+        
+        // ordinare l'array in base ai punti in modo decrescente
+        // riferimento : https://www.w3schools.com/php/php_arrays_sort.asp
+
+          
+        //ora genero l'html con la classifica ordinata 
+
+     
+
+    ?>
+
+    
+    </main>
+    <footer></footer>
+  </body>
+</html>
---
+++ 05_ESERCIZIO_classifica_con_array_associativi/README.md
@@ -0,0 +1,13 @@
+Riscrivere l'esempio 02_classifica usando un array associativo.
+```php
+        $classifica = array(
+            "Juventus" => 12,
+            "Inter"    => 45,
+            "Milan"    => 20,
+            "Roma"     => 21,
+            "Napoli"   => 33,
+            "Lazio"    => 18,
+            "Fiorentina" => 25)
+```
+riferimenti: ordinare array associativi [https://www.w3schools.com/php/php_arrays_sort.asp](https://www.w3schools.com/php/php_arrays_sort.asp)
+
